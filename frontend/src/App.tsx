0453e7a9968e70517ae0e0b08f6a62e8d3e17b41
Teerapon J <Teerapon.jeamphue@bangkokbank.com>
2026-07-22 00:27:32 +0700

    feat : change home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Navbar } from '@presentation/components/layout/Navbar'
-import { HomePage } from '@presentation/views/HomePage'
+// import { HomePage } from '@presentation/views/HomePage'
 import { ResumePage } from '@presentation/views/ResumePage'
 import { ResumeModernPage } from '@presentation/modern/ResumeModernPage'
 
@@ -20,14 +20,15 @@ export default function App() {
     <QueryClientProvider client={queryClient}>
       <HashRouter>
         <Routes>
-          <Route path="/resume_modern" element={<ResumeModernPage />} />
+          <Route path="/" element={<ResumeModernPage />} />
+          <Route path="/resume_modern" element={<Navigate to="/" replace />} />
           <Route
             path="*"
             element={
               <>
                 <Navbar />
                 <Routes>
-                  <Route path="/" element={<HomePage />} />
+                  {/* <Route path="/home" element={<HomePage />} /> */}
                   <Route path="/resume" element={<ResumePage />} />
                 </Routes>
               </>
